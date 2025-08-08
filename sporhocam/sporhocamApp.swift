611d21c9e7f873d7a08cfc4e7ce40247b6e4bd77
burakkho <burakkho@gmail.com>
2025-08-08 16:24:28 +0300

    Initial Commit

---
+++ sporhocam/sporhocamApp.swift
@@ -0,0 +1,32 @@
+//
+//  sporhocamApp.swift
+//  sporhocam
+//
+//  Created by Burak Macbook Mini on 8.08.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct sporhocamApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ sporhocamTests/sporhocamTests.swift
@@ -0,0 +1,17 @@
+//
+//  sporhocamTests.swift
+//  sporhocamTests
+//
+//  Created by Burak Macbook Mini on 8.08.2025.
+//
+
+import Testing
+@testable import sporhocam
+
+struct sporhocamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
